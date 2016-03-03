7f9af4da3332604fe6df6cb37374fb8b344e890a
Varun Date <date.varun123@gmail.com>
2016-03-04 01:33:26 +0530

    Revert "Revert appside bar (2/2)"
    This reverts commit 69e097d4a676be5cdd2f75dc47a8a68d35210876.

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -132,6 +132,7 @@ import com.android.settings.ButtonSettings;
 import com.android.settings.rr.MainSettings;
 import com.android.settings.rr.fragments.AppCircleBar;
 import com.android.settings.rr.gestureanywhere.GestureAnywhereSettings;
+import com.android.settings.rr.AppSidebar;
 import com.android.settings.rr.Pie.PieControl;
 import com.android.settings.vpn2.VpnSettings;
 import com.android.settings.wfd.WifiDisplaySettings;
@@ -299,6 +300,7 @@ public class SettingsActivity extends Activity
             R.id.main_settings,
 	    R.id.button_settings,
 	    R.id.app_circle_bar,
+	    R.id.app_sidebar,
 	    R.id.gesture_anywhere,
 	    R.id.pie_control,
             R.id.dashboard,
@@ -323,10 +325,11 @@ public class SettingsActivity extends Activity
             UserDictionarySettings.class.getName(),
             HomeSettings.class.getName(),
             MainSettings.class.getName(),
-            AppCircleBar.class.getName(),
-	        GestureAnywhereSettings.class.getName(),	
-            PieControl.class.getName(),	
-            ButtonSettings.class.getName(),
+	    AppCircleBar.class.getName(),
+	    AppSidebar.class.getName(),
+	    GestureAnywhereSettings.class.getName(),	
+	    PieControl.class.getName(),	
+	    ButtonSettings.class.getName(),
             DisplaySettings.class.getName(),
             DeviceInfoSettings.class.getName(),
             ManageApplications.class.getName(),
